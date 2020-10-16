6086c0d9aeec8f6a45a28fc232872a335cee6dc7
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-16 09:00:50 -0700

    add validations for review and product

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :product
+  validates :author, presence: true
+  validates_length_of :content_body, minimum: 50, maximum: 250
+  validates :rating, numericality: { greater_than_or_equal_to: 1}
+  validates :rating, numericality: { less_than_or_equal_to: 5}
+end

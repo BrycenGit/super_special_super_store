a9095242574b6162640299c65068020b43e788d3
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-16 14:02:12 -0700

    get scope to work for most reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,13 @@ class Product < ApplicationRecord
 
   scope :three_recent, -> { order(created_at: :desc).limit(3) }
 
+  scope :most_reviews, -> { (
+    select("products.id, products.name, products.cost, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)) }
+
   before_save(:titleize_product)
 
   private
--- app/models/review.rb
+++ app/models/review.rb
@@ -8,10 +8,5 @@ class Review < ApplicationRecord
   validates :rating, numericality: { less_than_or_equal_to: 5}\
   
 
-  scope :most_reviews, -> { (
-  select("products.id, products.name, products.price, count(reviews.id) as reviews_count")
-  .joins(:reviews)
-  .group("products.id")
-  .order("reviews_count DESC")
-  .limit(1)) }
+
 end

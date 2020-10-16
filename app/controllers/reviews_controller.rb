1fa23991c7c2f0a8d41fc3c1d56a683bd00f9a37
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-16 13:17:08 -0700

    fix path bug

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -30,6 +30,7 @@ class ReviewsController < ApplicationController
 
   def update
     @review = Review.find(params[:id])
+    @product = @review.product
     if @review.update(review_params)
       flash[:notice] = "review successfully updated!"
       redirect_to product_path(@review.product)
